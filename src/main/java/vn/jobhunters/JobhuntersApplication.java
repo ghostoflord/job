1a6b65b87646c2001d4e9aaf4ae58874ff25d6d0
ghostoflord <ghost2892003zzz@gmail.com>
2024-10-01 14:26:52 +0700

    creater project

---
+++ src/main/java/vn/jobhunters/JobhuntersApplication.java
@@ -0,0 +1,17 @@
+package vn.jobhunters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//disable security
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
+public class JobhuntersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobhuntersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/jobhunters/JobhuntersApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.jobhunters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobhuntersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
