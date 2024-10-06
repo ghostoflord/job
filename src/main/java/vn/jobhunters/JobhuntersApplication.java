a9b5ed58623feaf6ca9adf1cba2eb166cbd467c8
ghostoflord <ghost2892003zzz@gmail.com>
2024-10-06 10:46:19 +0700

    updtae creater project

--- src/main/java/vn/jobhunters/JobhuntersApplication.java
+++ src/main/java/vn/jobhunters/JobhuntersApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //disable security
 @SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+		// org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
 })
 public class JobhuntersApplication {
 
